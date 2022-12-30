4447c375ef0ed0b0ef47759ff209d6db4bbd14bb
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-12-30 01:36:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -20,5 +20,5 @@ def create_something(item: Item):
     htmlText = item.dict()['html']
     return "Set html text sucessfully."
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=8000)
